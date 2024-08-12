12d52b70c8499519e7443bc5b406a3bad6cc2812
Karn <karnssk123@gmail.com>
2024-08-12 12:04:33 +0700

    database fetching, all data are complete

--- client/.next/server/server-reference-manifest.js
+++ client/.next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"X8HNKLDQiGzJYnx/BGxPoch5kVSUWDVMtPdTgYhvOCc=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"GSg7LNPb+OPouLXAzhQD6Eu4STFns5L3LpR2hvSALac=\"\n}"
--- client/.next/static/chunks/webpack.js
+++ client/.next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "c50509c992cba01c"; }
+/******/ 		__webpack_require__.h = function() { return "2837ebcb8cedb466"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
