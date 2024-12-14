3ca89e2c5649f20c0d36635f0a0dfaf7a88a5575
Karn <karnssk123@gmail.com>
2024-12-14 16:41:11 +0700

    update

--- client/.next/server/server-reference-manifest.js
+++ client/.next/server/server-reference-manifest.js
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"GSg7LNPb+OPouLXAzhQD6Eu4STFns5L3LpR2hvSALac=\"\n}"
-=======
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"1OpyKOKWBPUoi0qmowFomTRDEbP0j4vjTln/bs9saT4=\"\n}"
->>>>>>> origin/Frontend-Test
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"\n}"
--- client/.next/server/webpack-runtime.js
+++ client/.next/server/webpack-runtime.js
@@ -130,11 +130,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-<<<<<<< HEAD
-/******/ 		__webpack_require__.h = () => ("c560f140b4bbe3b5")
-=======
-/******/ 		__webpack_require__.h = () => ("e13546ea667dcb76")
->>>>>>> origin/Frontend-Test
+/******/ 		__webpack_require__.h = () => ("02cc94c9273c2f4b")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
