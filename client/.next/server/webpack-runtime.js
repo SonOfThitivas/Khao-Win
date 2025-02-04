ace4fe0d06727d2f376985350bd67d0488ddfc54
Karn <karnssk123@gmail.com>
2025-02-04 12:21:12 +0700

    tap to continue update

--- client/.next/server/webpack-runtime.js
+++ client/.next/server/webpack-runtime.js
@@ -125,7 +125,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	(() => {
-/******/ 		__webpack_require__.h = () => ("32d2c494b864eaaa")
+/******/ 		__webpack_require__.h = () => ("3aaaa8a05e778131")
 /******/ 	})();
 /******/ 	
 /******/ 	/* webpack/runtime/hasOwnProperty shorthand */
--- client/src/app/page.js
+++ client/src/app/page.js
@@ -1,21 +1,21 @@
 // app/page.js
 "use client";
 import React from 'react';
-import Link from 'next/link';
 import Image from 'next/image';
-import { useRouter } from 'next/router';
 
 function Page() {
   return (
     <div className="logoin">
+    <a href="/home">
       <div className='headerpage'>
         <Image src="/LOGO1.png" alt="logo" width={400} height={400} className="center"/>
       </div>
       <div className="conti">
         <p>
-          <Link href="/home">Tap To Continue</Link>
+            Tap To Continue
         </p>
       </div>
+      </a>
       <div className='footerpage'>
         <p>Contact or Support developers</p>
         <p><a href="mailto:khaowinreport@gmail.com">khaowinreport@gmail.com</a></p>
